3039e650252e93c5d01516bc495bf9289dbae0d1
Asman <iasman312@gmail.com>
2021-05-20 13:23:04 +0600

    Добавили представления для решения задач

---
+++ django_project/api_v1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from api_v1.views import get_token_view, add_view, subtract_view, multiply_view, divide_view
+
+urlpatterns = [
+    path('add/', add_view, name='add'),
+    path('subtract/', subtract_view, name='subtract'),
+    path('multiply/', multiply_view, name='subtract'),
+    path('divide/', divide_view, name='subtract'),
+    path('get_token/', get_token_view, name='get-token')
+]
--- django_project/api_v1/views.py
+++ django_project/api_v1/views.py
@@ -1,3 +1,86 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-# Create your views here.
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
+
+
+def add_view(request, *args, **kwargs):
+    answer = {
+        'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'method': request.method,
+    }
+    if request.body:
+        body = json.loads(request.body)
+        if len(body.keys()) == 2:
+            if (isinstance(body['A'], str) or isinstance(body['B'], str)):
+                answer['error'] = 'Not a number'
+            else:
+                result = body['A'] + body['B']
+                answer['answer'] = result
+        else:
+            answer['error'] = 'Enter 2 numbers'
+    return JsonResponse(answer)
+
+
+def subtract_view(request, *args, **kwargs):
+    answer = {
+        'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'method': request.method,
+    }
+    if request.body:
+        body = json.loads(request.body)
+        if len(body.keys()) == 2:
+            if (isinstance(body['A'], str) and isinstance(body['B'], str)):
+                answer['error'] = 'Not a number'
+            else:
+                result = body['A'] - body['B']
+                answer['answer'] = result
+        else:
+            answer['error'] = 'Enter 2 numbers'
+    return JsonResponse(answer)
+
+
+def multiply_view(request, *args, **kwargs):
+    answer = {
+        'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'method': request.method,
+    }
+    if request.body:
+        body = json.loads(request.body)
+        if len(body.keys()) == 2:
+            if (isinstance(body['A'], str) and isinstance(body['B'], str)):
+                answer['error'] = 'Not a number'
+            else:
+                result = body['A'] * body['B']
+                answer['answer'] = result
+        else:
+            answer['error'] = 'Enter 2 numbers'
+    return JsonResponse(answer)
+
+
+def divide_view(request, *args, **kwargs):
+    answer = {
+        'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'method': request.method,
+    }
+    if request.body:
+        body = json.loads(request.body)
+        if len(body.keys()) == 2:
+            if (isinstance(body['A'], str) or isinstance(body['B'], str)):
+                answer['error'] = 'Not a number'
+            else:
+                if body['B'] != 0:
+                    result = body['A'] / body['B']
+                    answer['answer'] = result
+                else:
+                    answer['error'] = 'Division by 0'
+        else:
+            answer['error'] = 'Enter 2 numbers'
+    return JsonResponse(answer)
